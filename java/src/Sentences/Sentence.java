0ddc4d167fc3546457694afa506c5940f85b5b60
siongriffiths <sig2@aber.ac.uk>
2014-03-23 10:24:31 +0000

    JDOcs

--- java/src/Sentences/Sentence.java
+++ java/src/Sentences/Sentence.java
@@ -6,16 +6,23 @@ import Control.Stream;
  * @author Siôn Griffiths - sig2@aber.ac.uk
  *         Date: 09/03/14
  *         Time: 12:37
+ * The Sentence class is an abstract class which represents sn NMEA sentence.
  */
+
 public abstract class Sentence {
 
+  /**
+   * Array of tokenised sentence strings.
+   */
   protected String[] sentenceData;
 
+  /**
+   * Subclasses will implement this method to update a given stream
+   * @param stream The stream instance
+   */
   protected abstract void updateStream(Stream stream);
 
-  protected String[] getSentenceData(){
-    return sentenceData;
-  }
+
 
 
 }
--- java/src/Sentences/ZDA.java
+++ java/src/Sentences/ZDA.java
@@ -6,22 +6,41 @@ import Control.Stream;
  * @author Siôn Griffiths - sig2@aber.ac.uk
  *         Date: 09/03/14
  *         Time: 12:38
+ *  The ZDA class extracts time/date information from an NMEA ZDA sentence
  */
 public class ZDA extends Sentence{
 
+  /**
+   * Constructs a ZDA
+   * @param input array of tokenised NMEA sentence.
+   * @param stream The Stream instance containing the sentence
+   */
   public ZDA(String[] input,  Stream stream){
     sentenceData = input;
     updateStream(stream);
   }
 
+  /**
+   *
+   * @return String representation of the Stream time.
+   */
   public String makeTime(){
     return sentenceData[1];
   }
 
+  /**
+   *
+   * @return String representation of the Stream date.
+   */
   public String makeDate(){
     return sentenceData[2] + sentenceData[3] + sentenceData[4].charAt(2) + sentenceData[4].charAt(3);
   }
 
+  /**
+   * Updates the Stream instance. Passes time and date values
+   * back to the Stream
+   * @param stream the Stream instance
+   */
   @Override
   protected void updateStream(Stream stream) {
     stream.setTime(makeTime());
